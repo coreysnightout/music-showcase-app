61041049a33961815dd0d2148245b1ec8ff92b59
calvin cushman, corey redding and justin gomez <calvin+corey+justin@>
2017-06-15 11:40:17 -0700

    add javascript to push album information into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-alert("test");
+//back-end//
+function Album(artist, title, image) {
+  this.artist = artist;
+  this.newTitle = title;
+  this.image = image;
+}
+
+var albumArray = [];
+
+function resetFields() {
+  $("#artist-input").val("");
+  $("#album-input").val("");
+  $("#photo-input").val("");
+}
+
+// user interface//
+$(document).ready(function() {
+  $(".music-form").submit(function(event) {
+    event.preventDefault();
+    debugger;
+
+    var title = $("#album-input").val();
+    var artist = $("#artist-input").val();
+    var image =  $("#photo-input").val();
+
+    var newAlbum = new Album(artist, title, image);
+
+    console.log(newAlbum);
+
+    albumArray.push(newAlbum);
+  })
+})
